35e3e7d801d0684170c5cd67c4fbf89a6affdd3e
Lwimix <martilwinx@gmail.com>
2023-12-31 17:02:36 +0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """Prints Hello HBNB"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
