eea7d2208d418b12a39d7cc5f09b064a6c11b57f
Lwimix <martilwinx@gmail.com>
2024-01-02 01:10:56 +0300

    Flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def list_states():
     """Prints state objects"""
-    states = storage.all(State)
+    states = storage.all('State')
     sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', my_states=sorted_states)
 
@@ -18,6 +18,7 @@ def list_states():
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes session"""
+    storage.reload()
     storage.close()
 
 
